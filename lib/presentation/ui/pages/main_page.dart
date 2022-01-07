1d2305684656f514ecbffa4fc9963202656ac229
jmapps <jmappsmuslim@gmail.com>
2022-01-07 11:40:38 +0300

    Added bottom salomon navigation bar

--- lib/presentation/ui/pages/main_page.dart
+++ lib/presentation/ui/pages/main_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:project_fatiha/presentation/ui/widgets/salomon_bottom_navigation_bar.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.teal[100],
+    return Scaffold(
+      backgroundColor: Color(0xFFC4D4CA),
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          color: Color(0xFF6F867E),
+        ),
+        child: const SalomonBottomNavigationBar(),
+      ),
     );
   }
 }
---
+++ lib/presentation/ui/widgets/salomon_bottom_navigation_bar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+class SalomonBottomNavigationBar extends StatelessWidget {
+  const SalomonBottomNavigationBar({Key? key}) : super(key: key);
+
+  final int _currentIndex = 3;
+
+  @override
+  Widget build(BuildContext context) {
+    return SalomonBottomBar(
+        itemShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12.5),
+        ),
+        selectedItemColor: const Color(0xFFFFFFFF),
+        unselectedItemColor: const Color(0xFFC4D4CA),
+        margin: const EdgeInsets.all(16),
+        currentIndex: _currentIndex,
+        items: [
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.add),
+            title: Text('One'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.add),
+            title: Text('Two'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.add),
+            title: Text('Three'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.add),
+            title: Text('Four'),
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.add),
+            title: Text('Five'),
+          ),
+        ]);
+  }
+}
