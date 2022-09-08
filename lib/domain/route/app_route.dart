51b940a7ebcf8bf438c90a4fa7d9eb789f68616a
jmapps <jmappsmuslim@gmail.com>
2022-09-08 16:01:19 +0300

    Added main menu page's

---
+++ lib/domain/route/app_route.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:project_fatiha/presentation/pages/about_surah_page.dart';
+import 'package:project_fatiha/presentation/pages/read_error_page.dart';
+import 'package:project_fatiha/presentation/pages/subtleties_surah_page.dart';
+import 'package:project_fatiha/presentation/pages/tajweed_surah_page.dart';
+
+class AppRoute {
+  Route onGeneratorRoute(routeSettings) {
+    switch (routeSettings.name) {
+      case 'about_surah_page':
+        return MaterialPageRoute(
+          builder: (_) => const AboutSurahPage(),
+          settings: routeSettings,
+        );
+      case 'tajweed_surah_page':
+        return MaterialPageRoute(
+          builder: (_) => const TajweedSurahPage(),
+          settings: routeSettings,
+        );
+      case 'subtleties_surah_page':
+        return MaterialPageRoute(
+          builder: (_) => const SubtletiesSurahPage(),
+          settings: routeSettings,
+        );
+      case 'read_error_page':
+        return MaterialPageRoute(
+          builder: (_) => const ReadErrorPage(),
+          settings: routeSettings,
+        );
+      case 'management_app_page':
+        return MaterialPageRoute(
+          builder: (_) => const AboutSurahPage(),
+          settings: routeSettings,
+        );
+      default: throw (Exception('Invalid route ${routeSettings.name}'));
+    }
+  }
+}
---
+++ lib/presentation/pages/subtleties_surah_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SubtletiesSurahPage extends StatelessWidget {
+  const SubtletiesSurahPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
